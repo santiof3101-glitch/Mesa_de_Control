9569d7d91297b147d816a0883749806a88d323c2
santiof3101-glitch <santiof3101@gmail.com>
2026-07-15 00:55:18 -0500

    Rediseñar mesa de control ejecutivo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const STORAGE_KEY = "autocor-control-legal";
-const APP_BUILD_VERSION = "20260715-commercial-legal-flow";
+const APP_BUILD_VERSION = "20260715-legal-desk-executive";
 const TASK_RECONCILE_VERSION_KEY = "autocor-task-reconcile-version";
 const SUPABASE_URL = "https://evblnxgeyelatdmloydl.supabase.co/rest/v1";
 const SUPABASE_KEY = "sb_publishable_lFsurzFERQn1kQlfSsz1rA_588-DHwk";
